f26c139931d42af6a6ea62df54d4275b84f7bbf2
vinuaravindh <vinuaravindh@gmail.com>
2023-05-05 12:11:13 -0600

    added dark mode(should change colours)

---
+++ script.js
@@ -0,0 +1,19 @@
+sayHello();
+
+function sayHello() {
+    console.log("Hello World!");
+}
+
+const btn = document.querySelector(".footer-box button");
+const quote = document.querySelector(".mid-section");
+const head = document.querySelector(".header");
+const welc = document.querySelector(".welcome");
+
+ogcolor = quote.style.backgroundColor;
+
+btn.addEventListener("click", () => {
+    quote.classList.toggle('light-quote');
+    document.body.classList.toggle('dark');
+    head.classList.toggle('light-head');
+    welc.classList.toggle('light-head');
+})
